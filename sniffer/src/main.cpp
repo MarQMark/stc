ffd26ca3193fcee41a3e783659c4742b3c3bc0a1
mark <kugelflash@gmail.com>
2024-03-17 21:04:26 +0100

    Add Kikan CMake stuff

--- sniffer/src/main.cpp
+++ sniffer/src/main.cpp
@@ -1,7 +1,13 @@
+#include "Kikan/Engine.h"
 
 int main(){
 
+    Kikan::Engine::init();
+    Kikan::Engine* engine = Kikan::Engine::Kikan();
 
+    while (engine->shouldRun()) {
+        engine->update();
+    }
 
     return 0;
 }
